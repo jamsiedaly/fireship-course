a83a484c255f3dbd0014da4b9d9fdaf76fa3db8d
James Daly <jamsiedaly@gmail.com>
2023-10-25 13:04:10 +0100

    Created a basic login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,27 @@
-<h1>Sign up</h1>
+<script lang="ts">
+    export let data
+    let { supabase } = data
+    $: ({ supabase } = data)
+
+    const handleSignUp = async () => {
+        await supabase.auth.signInWithOAuth({
+            provider: 'google',
+        })
+    }
+
+    const handleSignOut = async () => {
+        await supabase.auth.signOut()
+    }
+</script>
+
+{#if !data.session}
+    <h1>Sign In</h1>
+    <form on:submit="{handleSignUp}">
+        <button class="btn btn-primary">Sign in with Google</button>
+    </form>
+{:else}
+    <h2 class="card-title">Welcome, {data.session.user.email}</h2>
+    <p class="text-center text-success">You are logged in</p>
+    <a href="/login/username"><button class="btn btn-primary">continue</button> </a>
+    <button class="btn btn-warning" on:click={handleSignOut}>Sign out</button>
+{/if}
